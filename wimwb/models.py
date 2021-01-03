de429089281b3abb6a57f78e33a5d41035434991
oiwn <alex@imscraping.ninja>
2021-01-03 23:24:59 +0700

    working now

--- wimwb/models.py
+++ wimwb/models.py
@@ -1,12 +1,53 @@
-from datetime import datetime
+# pylint: disable=too-few-public-methods
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.mysql import DATETIME, LONGTEXT
+from sqlalchemy.ext.declarative import declarative_base
 
-from pydantic import BaseModel
+
+Base = declarative_base()
+metadata = Base.metadata
 
 
-class UserAgentShort(BaseModel):
-    """Define minimal required useragent record"""
-    id: int
-    user_agent: str
-    times_seen: int
-    first_seen_at: datetime
-    updated_at: datetime
+class WhatismybrowserUseragent(Base):
+    """Created automatically via `sqlacodegen`"""
+    __tablename__ = 'whatismybrowser_useragent'
+
+    id = Column(Integer, primary_key=True)
+    user_agent = Column(LONGTEXT)
+    times_seen = Column(Integer, nullable=False)
+    simple_software_string = Column(String(255))
+    simple_sub_description_string = Column(String(255))
+    simple_operating_platform_string = Column(String(255))
+    software = Column(String(255))
+    software_name = Column(String(255))
+    software_name_code = Column(String(255))
+    software_version = Column(String(255))
+    software_version_full = Column(String(255))
+    operating_system = Column(String(255))
+    operating_system_name = Column(String(255))
+    operating_system_name_code = Column(String(255))
+    operating_system_version = Column(String(255))
+    operating_system_version_full = Column(String(255))
+    operating_system_flavour = Column(String(255))
+    operating_system_flavour_code = Column(String(255))
+    operating_system_frameworks = Column(LONGTEXT)
+    operating_platform = Column(String(255))
+    operating_platform_code = Column(String(255))
+    operating_platform_code_name = Column(String(255))
+    operating_platform_vendor_name = Column(String(255))
+    software_type = Column(String(255))
+    software_sub_type = Column(String(255))
+    software_type_specific = Column(String(255))
+    hardware_type = Column(String(255))
+    hardware_sub_type = Column(String(255))
+    hardware_sub_sub_type = Column(String(255))
+    hardware_type_specific = Column(String(255))
+    layout_engine_name = Column(String(255))
+    layout_engine_version = Column(String(255))
+    extra_info = Column(LONGTEXT)
+    extra_info_dict = Column(LONGTEXT)
+    capabilities = Column(LONGTEXT)
+    detected_addons = Column(LONGTEXT)
+    first_seen_at = Column(DATETIME(fsp=6), nullable=False)
+    last_seen_at = Column(DATETIME(fsp=6), nullable=False)
+    updated_at = Column(DATETIME(fsp=6))
--- wimwb/web.py
+++ wimwb/web.py
@@ -1,17 +1,43 @@
-from typing import Optional
-
+import databases
+import sqlalchemy
 from fastapi import FastAPI
+from pydantic import BaseSettings
 
+
+class Settings(BaseSettings):
+    """Application settings"""
+    app_name: str = "Whatismywebbrowser database API"
+    database_uri: str
+
+    class Config:  # pylint: disable=too-few-public-methods
+        """Automatically load settings from .env file"""
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+# setup things
+settings = Settings()
+database = databases.Database(settings.database_uri)
+metadata = sqlalchemy.MetaData()
+engine = sqlalchemy.create_engine(settings.database_uri)
+
+# setup application
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    """Docstring"""
-    return {"Hello": "World"}
+@app.on_event("startup")
+async def startup():
+    await database.connect()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, item: Optional[str] = None):
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/user_agents/")
+async def random_user_agents():
     """Docstring"""
-    return {"item_id": item_id, "item": item}
+    query = "SELECT * FROM whatismybrowser_useragent LIMIT 5"
+    rows = await database.fetch_all(query=query)
+    return {"user_agents": rows}
